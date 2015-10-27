0b7bcdf6e3b856783dacc43c09340a320dfbc294
Debian <admin@ip-172-31-20-12.ec2.internal>
2015-10-27 11:10:14 +0000

    Version 2.4, getting better and better. Now i preserve log symlink

--- installNeo4jEnterprise.sh
+++ installNeo4jEnterprise.sh
@@ -5,7 +5,7 @@ wget -O neo4j-enterprise-2.3.0-unix.tar.gz  http://neo4j.com/artifact.php?name=n
 tar -xvzf neo4j-enterprise-2.3.0-unix.tar.gz                                                                          && \
 cd neo4j-enterprise-2.3.0                                                                                             && \
 echo && echo "Please check all ok... Sleeping 30 seconds and then installing..."                                      && \
-echo "NOTE that conf and data directory will NOT be overwritten"                                                      && \
+echo "NOTE that conf directory will NOT be overwritten, but data will be moved in .OLD"                               && \
 sleep 30                                                                                                              && \
 echo && echo "Stopping neo4j server and copying new files..."                                                         && \
 sudo service neo4j stop                                                                                               && \
@@ -15,6 +15,8 @@ sudo rm -rf /var/lib/neo4j/bin        && sudo mv bin/ /var/lib/neo4j/           
 sudo rm -rf /usr/share/neo4j/lib      && sudo mv lib/ /usr/share/neo4j/           && sudo chown -R neo4j:adm /usr/share/neo4j    && \
 sudo rm -rf /usr/share/neo4j/plugins  && sudo mv plugins/ /usr/share/neo4j/       && sudo chown -R neo4j:adm /usr/share/neo4j/   && \
 sudo rm -rf /usr/share/neo4j/system   && sudo mv system/ /usr/share/neo4j/        && sudo chown -R neo4j:adm /usr/share/neo4j/   && \
+sudo rm -rf /var/lib/neo4j/data/*                                                                                                && \
+sudo ln -s  /var/log/neo4j/ /var/lib/neo4j/data/log                                                                              && \
 echo && echo "Please check all ok... Sleeping 30 seconds and then restaring neo4j server..."   && \
 sleep 30                                                                               && \
 echo && echo "Restaring neo4j server..."                                               && \
