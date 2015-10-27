bfd537dfaec181f23a632c2ed41c239d58927e29
Debian <admin@ip-172-31-20-12.ec2.internal>
2015-10-27 10:18:22 +0000

    Version 2, some better...

--- installNeo4jEnterprise.sh
+++ installNeo4jEnterprise.sh
@@ -1,14 +1,20 @@
+cd /tmp
+
+rm -rf neo4j-enterprise-2.3.0-unix.tar.gz    neo4j-enterprise-2.3.0                                                   && \
 wget -O neo4j-enterprise-2.3.0-unix.tar.gz  http://neo4j.com/artifact.php?name=neo4j-enterprise-2.3.0-unix.tar.gz     && \
 tar -xvzf neo4j-enterprise-2.3.0-unix.tar.gz                                                                          && \
 cd neo4j-enterprise-2.3.0                                                                                             && \
-echo "Please check all ok... Sleeping 30 seconds and then installing..."                                              && \
+echo && echo "Please check all ok... Sleeping 30 seconds and then installing..."                                      && \
 echo "NOTE that conf and data directory will NOT be overwritten"                                                      && \
 sleep 30                                                                                                              && \
+echo && echo "Stopping neo4j server and copying new files..."                                                         && \
+sudo service neo4j stop                                                                                               && \
 sudo rm -rf /var/lib/neo4j/bin        && sudo mv bin/ /var/lib/neo4j/             && sudo chown -R neo4j:adm /var/lib/neo4j/bin  && \
 sudo rm -rf /usr/share/neo4j/lib      && sudo mv lib/ /usr/share/neo4j/           && sudo chown -R neo4j:adm /usr/share/neo4j    && \
-sudo rm -rf /usr/share/neo4j/plugins  && sudo mv plugins /usr/share/neo4j/plugins && sudo chown -R neo4j:adm /usr/share/neo4j/   && \
-sudo rm -rf /usr/share/neo4j/system   && sudo mv system /usr/share/neo4j/system   && sudo chown -R neo4j:adm /usr/share/neo4j/   && \
-echo "Please check all ok... Sleeping 30 seconds and then restaring neo4j server..."   && \
+sudo rm -rf /usr/share/neo4j/plugins  && sudo mv plugins/ /usr/share/neo4j/       && sudo chown -R neo4j:adm /usr/share/neo4j/   && \
+sudo rm -rf /usr/share/neo4j/system   && sudo mv system/ /usr/share/neo4j/        && sudo chown -R neo4j:adm /usr/share/neo4j/   && \
+echo && echo "Please check all ok... Sleeping 30 seconds and then restaring neo4j server..."   && \
 sleep 30                                                                               && \
+echo && echo "Restaring neo4j server..."                                               && \
 sudo service neo4j restart                                                             && \
 echo "That's all Folks!"
